8b9e13580da239811f0a59cf2e1b9efc6ebd4696
leonid.ratanoff <leonid.ratanoff@gmail.com>
2018-11-17 02:13:44 +0500

    KINOMAN-8 SearchView 4.0

--- mobile/src/main/java/ru/ratanov/mobile/MobileActivity.kt
+++ mobile/src/main/java/ru/ratanov/mobile/MobileActivity.kt
@@ -1,14 +1,26 @@
 package ru.ratanov.mobile
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.NavHost
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.lapism.searchview.SearchAdapter
+import com.lapism.searchview.SearchItem
+import com.lapism.searchview.SearchView
+import com.lapism.searchview.SearchView.OnOpenCloseListener
+import kotlinx.android.synthetic.main.activity_mobile.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import ru.ratanov.core.repo.FilmRepository
+
 
 class MobileActivity : AppCompatActivity(), NavHost {
     override fun getNavController(): NavController {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return Navigation.findNavController(this, R.id.my_nav_host_fragment)
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -19,6 +31,18 @@ class MobileActivity : AppCompatActivity(), NavHost {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mobile)
+
+        setupSearchView()
+
+//        NavigationUI.setupWithNavController(toolbar, navController)
+
+        fab.setOnClickListener {
+            if (searchview.isSearchOpen) {
+                searchview.close(true)
+            } else {
+                searchview.open(true)
+            }
+        }
     }
 
     override fun onBackPressed() {
@@ -30,7 +54,33 @@ class MobileActivity : AppCompatActivity(), NavHost {
     }
 
 
+    private fun setupSearchView() {
+        with(searchview) {
+            version = SearchView.VERSION_MENU_ITEM
+            setVersionMargins(SearchView.VERSION_MARGINS_MENU_ITEM)
+            setTheme(SearchView.THEME_LIGHT)
+            setOnOpenCloseListener(object : OnOpenCloseListener {
+                override fun onOpen(): Boolean {
+                    fab.hide()
+                    return true
+                }
 
+                override fun onClose(): Boolean {
+                    fab.show()
+                    return true
+                }
+
+            })
+
+            doAsync {
+                val suggestion = FilmRepository.getTopFilms()?.map { SearchItem(it.title) }
+                uiThread {
+                    adapter = SearchAdapter(this@MobileActivity, suggestion)
+                }
+            }
+
+        }
+    }
 
 
 }
