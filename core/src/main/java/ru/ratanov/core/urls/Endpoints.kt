2663213300d1d3d3e24bafff2f39d8095129766b
ratanoff <leonid.ratanoff@gmail.com>
2019-02-02 00:56:16 +0500

    change url to home test server

--- core/src/main/java/ru/ratanov/core/urls/Endpoints.kt
+++ core/src/main/java/ru/ratanov/core/urls/Endpoints.kt
@@ -3,6 +3,6 @@ package ru.ratanov.core.urls
 
 object Endpoints {
 
-    private const val BASE_URL = "http://kinoman.tech:8081/api"
+    private const val BASE_URL = "http://46.146.211.31:8081/api"
     const val TOP = "$BASE_URL/top"
 }
